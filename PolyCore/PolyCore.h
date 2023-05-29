b5a39b19c6a2c5f8f2e70bcf520f7252f9ec7a05
Kevin Dang <overcyn@gmail.com>
2023-05-29 13:20:26 -0700

    Initial Commit

---
+++ PolyCore/PolyCore.h
@@ -0,0 +1,18 @@
+//
+//  PolyCore.h
+//  PolyCore
+//
+//  Created by overcyn on 5/29/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PolyCore.
+FOUNDATION_EXPORT double PolyCoreVersionNumber;
+
+//! Project version string for PolyCore.
+FOUNDATION_EXPORT const unsigned char PolyCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PolyCore/PublicHeader.h>
+
+
